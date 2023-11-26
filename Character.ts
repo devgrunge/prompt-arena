bf78c687a435106f23f27281555a92121c3bdafd
alexanderJr <alexmarcjr21@gmail.com>
2023-11-26 19:22:17 +0000

    feat: battle option

--- Character.ts
+++ Character.ts
@@ -20,9 +20,11 @@ let options: number = 0;
 
 while (options !== 9) {
   console.log("================= Character   =================");
-  console.log("| 1.train attack                              |");
-  console.log("| 2.train defense                             |");
-  console.log("| 3.Print attributes                          |");
+  console.log("| 1.Train attack                              |");
+  console.log("| 2.Train defense                             |");
+  console.log("| 3.Rest                                      |");
+  console.log("| 4.Enter in combat                           |");
+  console.log("| 8.Print attributes                          |");
   console.log("| 9.Exit                                      |");
   console.log("===============================================");
 
@@ -30,9 +32,22 @@ while (options !== 9) {
 
   switch (options) {
     case 1:
-      character.attack += 2;
+      character.attack += Math.random() * 7;
+      character.energy -= Math.random() * 10;
+      break;
+    case 2:
+      character.defense += Math.random() * 5;
+      character.energy -= Math.random() * 10;
       break;
     case 3:
+      character.energy += Math.random() * 10;
+      break;
+    case 4:
+      character.energy -= Math.random() * 100;
+      if (character.energy <= 0) {
+        console.log("Oops, you're dead");
+      }
+    case 8:
       console.log("Character ==> ", character);
       break;
     default:
