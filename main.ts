5c08fe383e6d222980bf2a6095256bfa64070cec
alexanderJr <alexmarcjr21@gmail.com>
2023-11-27 19:41:31 +0000

    feat: encapsuled constructor

---
+++ main.ts
@@ -0,0 +1,74 @@
+import prompt from "prompt-sync";
+import Character from "./src/Character";
+
+let character: Character = new Character("Sansa stark", 100, 50, 20, 30);
+
+let keyboard = prompt();
+
+let options: number = 0;
+
+while (options !== 9) {
+  console.log("================= Character   =================");
+  console.log("| 1.Train attack                              |");
+  console.log("| 2.Train defense                             |");
+  console.log("| 3.Rest                                      |");
+  console.log("| 4.Enter in combat                           |");
+  console.log("| 8.Print attributes                          |");
+  console.log("| 9.Exit                                      |");
+  console.log("===============================================");
+
+  options = +keyboard("Choose a option:  ");
+
+  switch (options) {
+    case 1:
+      character.attack += Math.random() * 7;
+      character.energy -= Math.random() * 10;
+      console.log("Name: ", character.name);
+      console.log("Energy: ", character.energy.toFixed(0));
+      console.log("Energy: ", character.attack.toFixed(0));
+      console.log("Energy: ", character.defense.toFixed(0));
+
+      break;
+    case 2:
+      character.defense += Math.random() * 5;
+      character.energy -= Math.random() * 10;
+      console.log("Name: ", character.name);
+      console.log("Energy: ", character.energy.toFixed(0));
+      console.log("Energy: ", character.attack.toFixed(0));
+      console.log("Energy: ", character.defense.toFixed(0));
+
+      break;
+    case 3:
+      character.energy += Math.random() * 10;
+      if (character.energy > 100) {
+        character.energy = 100;
+      }
+      console.log("Name: ", character.name);
+      console.log("Energy: ", character.energy.toFixed(0));
+      console.log("Energy: ", character.attack.toFixed(0));
+      console.log("Energy: ", character.defense.toFixed(0));
+
+      break;
+    case 4:
+      character.energy -= Math.random() * 100;
+      if (character.energy <= 0) {
+        console.log("Oops, you're dead");
+      }
+      console.log("Name: ", character.name);
+      console.log("Energy: ", character.energy.toFixed(0));
+      console.log("Energy: ", character.attack.toFixed(0));
+      console.log("Energy: ", character.defense.toFixed(0));
+
+      break;
+    case 8:
+      console.log("     Character:     ");
+      console.log("Name: ", character.name);
+      console.log("Energy: ", character.energy.toFixed(0));
+      console.log("Energy: ", character.attack.toFixed(0));
+      console.log("Energy: ", character.defense.toFixed(0));
+
+      break;
+    default:
+      console.log("Leaving game");
+  }
+}
---
+++ src/Character.ts
@@ -0,0 +1,9 @@
+export default class Character {
+  constructor(
+    public name: string,
+    public energy: number,
+    public hitPoints: number,
+    public attack: number,
+    public defense: number
+  ) {}
+}
