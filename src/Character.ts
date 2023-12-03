70d7a38dcc35ecbcf2ac304568dd3a400c699d11
alexanderJr <alexmarcjr21@gmail.com>
2023-12-03 20:51:53 +0000

    feat: getters and setters

--- src/Character.ts
+++ src/Character.ts
@@ -1,13 +1,21 @@
 export default class Character {
   constructor(
-    public name: string,
-    public energy: number,
-    public hitPoints: number,
-    public attack: number,
-    public defense: number
+    private _name: string,
+    private energy: number,
+    private hitPoints: number,
+    private attack: number,
+    private defense: number
   ) {}
 
-  status(): string {
+  public get name(): string {
+    return this._name;
+  }
+
+  public set name(newName: string) {
+    this._name = newName;
+  }
+
+  public status(): string {
     return (
       "Character  \n" +
       "Name: " +
@@ -21,34 +29,39 @@ export default class Character {
     );
   }
 
-  enterInCombat(): number {
-    let exhaustion = Math.random() * 100;
+  public enterInCombat(): number {
+    let exhaustion = this.randomizer(100);
     this.energy -= exhaustion;
     this.isDead();
     return this.energy;
   }
 
-  trainAttack(): void {
-    this.attack += Math.random() * 7;
-    this.energy -= Math.random() * 10;
+  public trainAttack(): void {
+    this.attack += this.randomizer(7);
+    this.energy -= this.randomizer(10);
     this.isDead();
   }
 
-  trainDefense(): void {
-    this.defense += Math.random() * 5;
-    this.energy -= Math.random() * 10;
+  public trainDefense(): number {
+    this.defense += this.randomizer(5);
+    this.energy -= this.randomizer(10);
     this.isDead();
+    return this.energy;
   }
 
-  rest(hours: number): void {
-    this.energy += hours * (Math.random() * 10);
+  public rest(hours: number): void {
+    this.energy += hours * this.randomizer(10);
     if (this.energy > 100) {
       this.energy = 100;
     }
     console.log("Name: ", this.name);
   }
 
-  isDead(): boolean {
+  public isDead(): boolean {
     return this.energy <= 0 ? false : true;
+  }
+
+  private randomizer(factor: number): number {
+    return Math.random() * factor;
   }
 }
